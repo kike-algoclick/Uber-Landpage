b834d1d662b97b5c213eabf57f434fd30bdb6369
Pimente1215 <madelinne.pineda@2026hilasal.superate.org.sv>
2025-07-14 20:26:04 -0600

    sombras de los botons

--- components/navbar.js
+++ components/navbar.js
@@ -5,17 +5,18 @@ class Navbar extends HTMLElement {
                 <div ><img  class="w-15 ml-12"  src="images/Uber-logo.png" alt="img" ></div>
                 
                 <div class="flex justify-start items-center gap-6 w-210 ">
-                <ul class="">Ride</ul>
-                <ul>Drive</ul>
-                <ul>Business</ul>
-                <ul>Uber Eats</ul>
-                <ul"> About</ul>
+                <ul class="hover:bg-gray-700 text-withe px-3 py-1 rounded-full cursor-pointer transition-all duration-200">Ride</ul>
+                <ul class="hover:bg-gray-700 text-withe px-3 py-1 rounded-full cursor-pointer transition-all duration-200">Drive</ul>
+                <ul class="hover:bg-gray-700 text-withe px-3 py-1 rounded-full cursor-pointer transition-all duration-200">Business</ul>
+                <ul class="hover:bg-gray-700 text-withe px-3 py-1 rounded-full cursor-pointer transition-all duration-200">Uber Eats</ul>
+                <ul class="hover:bg-gray-700 text-white px-3 py-1 rounded-full cursor-pointer transition-all duration-200 flex items-center gap-2"> About
+                <span><svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" /></svg></span></ul>
                 </div>
 
                 <div class="flex justify-start items-center gap-6">
-                <ul class="flex justify-center items-center gap-2">  <span style="font-size:18px ;" class=" material-symbols-outlined">Language</span> EN </ul>
-                <ul>Help</ul>
-                <ul>Login</ul>
+                <ul class="flex justify-center items-center gap-2">  <span style="font-size:18px ;" class=" material-symbols-outlined  hover:bg-gray-700 text-withe px-3 py-1 rounded-full cursor-pointer transition-all duration-200">Language</span> EN </ul>
+                <ul class="hover:bg-gray-700 text-withe px-3 py-1 rounded-full cursor-pointer transition-all duration-200">Help </ul>
+                <ul class="hover:bg-gray-700 text-withe px-3 py-1 rounded-full cursor-pointer transition-all duration-200">Login</ul>
                 <ul class="bg-white text-black p-2 rounded-full text-sm font-bold w-18 text-center">Sign Up</ul>
                 </div>
             </nav>`;
