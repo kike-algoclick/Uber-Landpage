7f21575d825b6010580e2fa5caf3e0257a1762e5
brayan-r1 <brayan.rivas2026@hilasal.superate.org.sv>
2025-07-14 21:49:29 -0600

    Footer terminado

--- components/footer.js
+++ components/footer.js
@@ -1,10 +1,10 @@
 class FooterUber extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
-      <footer class="bg-[#2c2c2c] text-white text-sm">
+      <footer class="bg-black text-white text-sm">
         <div class="max-w-7xl mx-auto px-6 py-10 grid grid-cols-1 md:grid-cols-4 gap-8">
           <div>
-            <img src="https://th.bing.com/th/id/R.ee430489d1505483166c19ab9ed00d4e?rik=9h%2bv%2fqv8kXnGKQ&pid=ImgRaw&r=0" alt="Uber" class="h-8 mb-6" />
+            <img src="https://th.bing.com/th/id/R.ec5cc17f72b177446d365a0c4c8b0c3d?rik=HuRiy31Fjivhrg&pid=ImgRaw&r=0" alt="Uber" class="h-30 mb-30 w-30" />
           </div>
           <div>
             <h4 class="font-bold mb-2">Nuestros servicios</h4>
@@ -38,16 +38,15 @@ class FooterUber extends HTMLElement {
           </div>
         </div>
 
-        <!-- Sección inferior -->
+       
         <div class="max-w-7xl mx-auto px-6 py-10 grid grid-cols-1 md:grid-cols-3 gap-8">
-          <!-- Izquierda -->
+         
           <div>
-            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f9/InDrive_logo_2022.svg/2560px-InDrive_logo_2022.svg.png" alt="inDrive" class="h-8 mb-4" />
             <div class="flex space-x-4 mb-4">
-              <a href="#"><img src="https://cdn-icons-png.flaticon.com/512/733/733603.png" class="h-6 w-6 rounded-full bg-lime-400 p-1" alt="Web" /></a>
-              <a href="#"><img src="https://cdn-icons-png.flaticon.com/512/733/733547.png" class="h-6 w-6 rounded-full bg-lime-400 p-1" alt="Facebook" /></a>
-              <a href="#"><img src="https://cdn-icons-png.flaticon.com/512/733/733558.png" class="h-6 w-6 rounded-full bg-lime-400 p-1" alt="Instagram" /></a>
-              <a href="#"><img src="https://cdn-icons-png.flaticon.com/512/733/733635.png" class="h-6 w-6 rounded-full bg-lime-400 p-1" alt="TikTok" /></a>
+              <a href="#"><img src="https://indrive.com/images/icons/wiki.svg" class="h-6 w-6 rounded-full bg-lime-400 p-1" alt="Web" /></a>
+              <a href="#"><img src="https://indrive.com/images/icons/fb.svg" class="h-6 w-6 rounded-full bg-lime-400 p-1" alt="Facebook" /></a>
+              <a href="#"><img src="https://indrive.com/images/icons/instagram.svg" class="h-6 w-6 rounded-full bg-lime-400 p-1" alt="Instagram" /></a>
+              <a href="#"><img src="https://indrive.com/images/icons/x.svg" class="h-6 w-6 rounded-full bg-lime-400 p-1" alt="TikTok" /></a>
             </div>
             <p class="text-gray-400 text-xs leading-relaxed">
               Uber facilita la conexión entre prestadores de servicios y clientes; no es un servicio de transporte público.
@@ -62,7 +61,7 @@ class FooterUber extends HTMLElement {
             </ul>
           </div>
 
-          <!-- Centro -->
+         
           <div class="grid grid-cols-2 gap-4 text-sm">
             <div class="space-y-1">
               <p><a href="#" class="hover:underline">Sostenibilidad</a></p>
@@ -84,8 +83,9 @@ class FooterUber extends HTMLElement {
               <span>Es</span>
             </div>
             <div class="flex space-x-2">
-              <a href="#"><img src="https://upload.wikimedia.org/wikipedia/commons/5/5f/Available_on_the_App_Store_(black)_SVG.svg" class="h-10" alt="App Store" /></a>
-              <a href="#"><img src="https://upload.wikimedia.org/wikipedia/commons/7/78/Google_Play_Store_badge_EN.svg" class="h-10" alt="Google Play" /></a>
+              <a href="#"><img src="https://indrive.com/images/icons/app-store.svg" class="h-10" alt="App Store" /></a>
+              <a href="#"><img src="https://indrive.com/images/icons/google-play.svg" class="h-10" alt="Google Play" /></a>
+               <a href="#"><img src="https://indrive.com/images/icons/app-andriod.svg" class="h-10" alt="Adroid" /></a>
             </div>
             <div class="text-xs text-gray-400">
               <p>© SUOL INNOVATIONS LTD, 2013–2025</p>
